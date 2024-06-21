18e1b58c7d139dcc1754991402a9a6f4a10b1a8a
aryan02420 <aryan02420@gmail.com>
2024-06-21 16:02:37 +0530

    feat: tauri deps

---
+++ src/tauri-debian-dependencies/NOTES.md
@@ -0,0 +1,3 @@
+## TODO
+
+- [ ] automatically detect os from os-release and use suitable install command. see: https://github.com/devcontainers/features/blob/main/src/node/install.sh
---
+++ src/tauri-debian-dependencies/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+set -e
+
+export TAURI_VERSION="${TAURIVERSION:-"latest"}"
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+if [ "${TAURI_VERSION}" == "1" ]; then
+    sudo apt update
+    sudo apt install -y \
+        libwebkit2gtk-4.0-dev \
+        build-essential \
+        curl \
+        wget \
+        file \
+        libssl-dev \
+        libgtk-3-dev \
+        libayatana-appindicator3-dev \
+        librsvg2-dev
+fi
+
+if [ "${TAURI_VERSION}" == "2" ] || [ "${TAURI_VERSION}" == "latest" ]; then
+    sudo apt update
+    sudo apt install -y \
+        libwebkit2gtk-4.1-dev \
+        build-essential \
+        curl \
+        wget \
+        file \
+        libxdo-dev \
+        libssl-dev \
+        libayatana-appindicator3-dev \
+        librsvg2-dev
+fi
